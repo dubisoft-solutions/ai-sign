253aca177dde6a088fef45e6e6ee2ab985aa5a5f
Ivan Dubynets <ivan.dubynets@gmail.com>
2023-06-27 19:37:26 +0200

    fixed mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,22 @@ import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <header className="border-bottom py-3">
-            <div className="container-fluid">
-                <nav className="nav nav-masthead justify-content-center float-end">
-                    <a href="#" className="nav-link fw-bold py-1 px-3 active">FAQ</a>
-                    <Link  to="/demo" className="nav-link fw-bold py-1 px-3">Demo</Link>
-                    <Link  to="/contact" className="nav-link fw-bold py-1 px-3">Contact us</Link>
-                </nav>
-            </div>
+        <header>
+            <nav className="navbar navbar-expand top-navbar navbar-dark fixed-top d-flex py-3 border-bottom">
+                <div className="container-fluid justify-content-end">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a href="#" className="nav-link py-1 px-3 active">FAQ</a>
+                        </li>
+                        <li className="nav-item">
+                            <Link  to="/demo" className="nav-link py-1 px-3">Demo</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link  to="/contact" className="nav-link py-1 px-3">Contact us</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </header>
     )
 }
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
 
-import herobg from '../assets/images/hero-bg.png';
+import owners from '../assets/images/owners.png';
+import companies from '../assets/images/companies.png';
+import users from '../assets/images/users.png';
+import arrowright from '../assets/images/arrow-right.svg';
 
 const IndexPage = () => {
 
     return  (
         <>
         <div className="hero d-flex w-100 h-100 mx-auto flex-column">
-            <main>
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-lg-7 text-center">
-                            <div className="pb-5">
-                                <a href="#">
-                                    <img src="src/assets/images/logo.svg" alt="Hero bg" />
-                                </a>
-                            </div>
-                            <h1 className="text-white mb-lg-4 mb-3">Protect and track your data</h1>
-                            <p className="text-white fs-1 mb-lg-5 mb-4">Create a traceable and permanent digital signature
-        for your content that withstands the challenges posed 
-        by generative AI models.<br></br> 
-        Works for images, videos, audio, and text.</p>
-                            <div className="d-lg-flex gap-2 d-grid">
-                                <a className="btn btn-outline-info w-100" href="#" role="button">Watch how it works</a>
-                                <a className="btn btn-info w-100" href="#" role="button">Try a demo</a>
-                            </div>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-lg-7 text-center">
+                        <div className="pb-5">
+                            <a href="#">
+                                <img src="src/assets/images/logo.svg" alt="Hero bg" />
+                            </a>
+                        </div>
+                        <h1 className="text-white mb-lg-4 mb-3">Protect and track your data</h1>
+                        <p className="text-white fs-2 mb-lg-5 mb-4">Create a traceable and permanent digital signature
+    for your content that withstands the challenges posed 
+    by generative AI models.<br></br> 
+    Works for images, videos, audio, and text.</p>
+                        <div className="d-lg-flex gap-2 d-grid">
+                            <a className="btn btn-outline-info w-100" href="#" role="button">Watch how it works</a>
+                            <a className="btn btn-info w-100" href="#" role="button">Try a demo</a>
                         </div>
                     </div>
                 </div>
-            </main>
+            </div>
         </div>
         <div className="models-section">
             <div className="container">
@@ -36,36 +37,42 @@ const IndexPage = () => {
                     <div className="col-lg-8 text-center">
                         <h2 className="mb-4">Your data is being used to train AI models,  
                         <span className="d-lg-block">without your knowledge or consent.</span></h2>
-                            <p className="fs-2 mb-lg-5 mb-4">Companies are stealing copyright, violating terms of use <span className="d-lg-block">and 
+                            <p className="fs-3 mb-lg-5 mb-4">Companies are stealing copyright, violating terms of use <span className="d-lg-block">and 
     misusing personal data, creating large datasets to train models.</span></p>
                     </div>
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-lg-12 text-center">
                         <div className="card mb-4">
-                            <div className="card-body row">
-                                <div className="col-lg-4 text-center pb-lg-0 pb-4">
+                            <div className="card-body d-lg-flex d-block align-items-center">
+                                <div className="w-100 text-center pb-lg-0 pb-4">
                                     <h3 className="text-secondary fw-normal mb-4">Data owners</h3>
-                                    <img src="src/assets/images/owners.png" alt="Data owners" />
-                                    <p className="fs-3 mt-3">Text, image, video, and audio data is stolen</p>
+                                    <img src={owners} alt="Data owners" />
+                                    <p className="fs-4 mt-3">Text, image, video, and audio data is stolen</p>
                                 </div>
-                                <div className="col-lg-4 text-center pb-lg-0 pb-4">
+                                <div className="flex-shrink-1 text-center pb-lg-0 pb-4 d-lg-block d-none">
+                                    <img src={arrowright} alt="Arrow right" />
+                                </div>
+                                <div className="w-100 text-center pb-lg-0 pb-4">
                                     <h3 className="text-secondary fw-normal mb-4">AI companies</h3>
-                                    <img src="src/assets/images/companies.png" alt="Companies" />
-                                    <p className="fs-3 mt-3">Microsoft, Github, Midjourney, OpenAI, and StabilityAI
+                                    <img src={companies} alt="Companies" />
+                                    <p className="fs-4 mt-3">Microsoft, Github, Midjourney, OpenAI, and StabilityAI
 train models for applications </p>
                                 </div>
-                                <div className="col-lg-4 text-center">
+                                <div className="flex-shrink-1 text-center pb-lg-0 pb-4 d-lg-block d-none">
+                                    <img src={arrowright} alt="Arrow right" />
+                                </div>
+                                <div className="w-100 text-center">
                                     <h3 className="text-secondary fw-normal mb-4">End Users</h3>
-                                    <img src="src/assets/images/users.png" alt="Users" />
-                                    <p className="fs-3 mt-3">Users of
+                                    <img src={users} alt="Users" />
+                                    <p className="fs-4 mt-3">Users of
 Github Copilot, ChatGPT, and StableDiffusion
  pay AI companies. Data owners get nothing.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="mb-4">
-                            <p className="fs-4">Can probably cite ongoing lawsuits</p>
+                            <p className="fs-5">Can probably cite ongoing lawsuits</p>
                             <a href="https://techcrunch.com/2023/01/27/the-current-legal-cases-against-generative-ai-are-just-the-beginning/" className="btn-link d-block">https://techcrunch.com/2023/01/27/the-current-legal-cases-against-generative-ai-are-just-the-beginning/</a>
                             <a href="https://www.theverge.com/2023/1/16/23557098/generative-ai-art-copyright-legal-lawsuit-stable-diffusion-midjourney-deviantart" className="btn-link d-block">https://www.theverge.com/2023/1/16/23557098/generative-ai-art-copyright-legal-lawsuit-stable-diffusion-midjourney-deviantart</a>
                             <a href="https://www.latimes.com/entertainment-arts/music/story/2023-04-18/drake-the-weeknd-ai-song-fake-removed-streaming-services" className="btn-link d-block">https://www.latimes.com/entertainment-arts/music/story/2023-04-18/drake-the-weeknd-ai-song-fake-removed-streaming-services</a>
@@ -83,29 +90,31 @@ Github Copilot, ChatGPT, and StableDiffusion
                                 <h2 className="text-start mb-0">How it works</h2>
                             </div>
                             <div className="card-body p-0">
-                                <div className="row d-flex align-items-center">
-                                    <div className="col-lg-6 pe-lg-0">
-                                        <img src={herobg} alt="Hero bg" className="img-fluid" />
+                                <div className="row">
+                                    <div className="col-lg-6 pe-lg-0 order-lg-0 order-0">
+                                        <div className="img-fluid">
+                                            <p className="fs-1 text-muted">.GIF<br></br> illustrating<br></br> this step</p>
+                                        </div>
                                     </div>
-                                    <div className="col-lg-6 px-5 text-start">
+                                    <div className="col-lg-6 px-5 py-lg-0 py-5 text-start order-lg-1 order-1 my-auto">
                                         <h3>Upload your digital art work, add a unique fingerprint</h3>
                                         <p>Upload your digital art work, add a unique fingerprint</p>
                                     </div>
-                                </div>
-                                <div className="row d-flex align-items-center">
-                                    <div className="col-lg-6 px-5 text-start">
+                                    <div className="col-lg-6 px-5 py-lg-0 py-5 text-start order-lg-2 order-3 my-auto">
                                         <h3>Download, and export your original artwork with our invisible fingerprint</h3>
                                         <p>Some more content here explaining how they art is now protected from AI modeling</p>
                                     </div>
-                                    <div className="col-lg-6 ps-lg-0">
-                                        <img src="src/assets/images/hero-bg.png" alt="Hero bg" className="img-fluid" />
+                                    <div className="col-lg-6 ps-lg-0 order-lg-3 order-2">
+                                        <div className="img-fluid">
+                                            <p className="fs-1 text-muted">.GIF<br></br> illustrating<br></br> this step</p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="row d-flex align-items-center">
-                                    <div className="col-lg-6 pe-lg-0">
-                                        <img src="src/assets/images/hero-bg.png" alt="Hero bg" className="img-fluid" />
+                                    <div className="col-lg-6 pe-lg-0 order-lg-4 order-4">
+                                        <div className="img-fluid">
+                                            <p className="fs-1 text-muted">.GIF<br></br> illustrating<br></br> this step</p>
+                                        </div>
                                     </div>
-                                    <div className="col-lg-6 px-5 text-start">
+                                    <div className="col-lg-6 px-5 py-lg-0 py-5 text-start order-lg-5 order-5 my-auto">
                                         <h3>Stay protected and prove your ownership</h3>
                                         <p>After securing your digital intellectual property, you’re able to verify and prove an AI model has utilized your art by uploading to our system and verifying your fingerprint matches the stolen work</p>
                                     </div>
