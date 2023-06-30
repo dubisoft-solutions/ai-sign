c7d33a02f8b9bc10a95e383c52fc935f14b08c10
Ivan Dubynets <ivan.dubynets@gmail.com>
2023-06-30 17:26:52 +0200

    add navbar mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { Outlet, Link } from "react-router-dom";
 
 import logonav from '../assets/images/logo-nav.svg';
+import menu from '../assets/images/menu.svg';
+import close from '../assets/images/close.svg';
 
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import { routeCodes, useMatchedRoute } from "../routes";
@@ -11,24 +13,34 @@ const Header = ({scrolled=false}) => {
 
     return (
         <header>
-            <nav className={`navbar navbar-expand top-navbar navbar-dark fixed-top d-flex py-3 border-bottom` + ((scrolled || showFullHeader) && ' scrolled')}>
+            <nav className={`navbar navbar-expand-lg top-navbar navbar-dark fixed-top d-flex py-3 border-bottom` + ((scrolled || showFullHeader) && ' scrolled')}>
                 <div className="container-fluid justify-content-end">
                     { showFullHeader && (
                         <Link to={routeCodes.HOME} className="navbar-brand">
                             <img src={logonav} alt="AI Sign" />
                         </Link>
                     ) }
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
-                            <a href="#" className="nav-link py-1 px-3 active">FAQ</a>
-                        </li>
-                        <li className="nav-item">
-                            <Link  to={routeCodes.DEMO} className="nav-link py-1 px-3">Demo</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link  to={routeCodes.CONTACT} className="nav-link py-1 px-3">Contact us</Link>
-                        </li>
-                    </ul>
+                    <button className="navbar-toggler collapsed border-0 px-0 ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="toggler-collapsed">
+                            <img src={menu} alt="Menu" />
+                        </span>
+                        <span className="toggler-expanded">
+                            <img src={close} alt="Close" />
+                        </span>
+                    </button>
+                    <div className="collapse navbar-collapse nav-menu-wrapper" id="navbarCollapse">
+                        <ul className="navbar-nav navigation-menu ms-auto">
+                            <li className="nav-item">
+                                <a href="#" className="nav-link py-1 px-3 active">FAQ</a>
+                            </li>
+                            <li className="nav-item">
+                                <Link  to={routeCodes.DEMO} className="nav-link py-1 px-3">Demo</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link  to={routeCodes.CONTACT} className="nav-link py-1 px-3">Contact us</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </header>
