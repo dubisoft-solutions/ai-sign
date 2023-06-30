b006ce199d820793d759aefd8c5aa5f739b8bb0c
Ivan Dubynets <ivan@dubisoft.solutions>
2023-06-30 11:59:12 +0200

    fixed the navbar style

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,3 +1,6 @@
+import { matchPath } from 'react-router';
+import { useLocation } from 'react-router-dom';
+
 const publicPath = '/';
 
 export const routeCodes = {
@@ -5,3 +8,18 @@ export const routeCodes = {
   DEMO: `${ publicPath }demo`,
   CONTACT: `${ publicPath }contact`,
 };
+
+
+export const routes = [
+    routeCodes.HOME, routeCodes.DEMO, routeCodes.CONTACT
+]
+
+
+export function useMatchedRoute() {
+  const { pathname } = useLocation();
+  for (const route of routes) {
+    if (matchPath({ path: route }, pathname)) {
+      return route;
+    }
+  }
+}
