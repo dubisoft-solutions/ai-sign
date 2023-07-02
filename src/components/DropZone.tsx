b34e93cf8d3057d023759c996983f0d2f9aa5088
Ivan Dubynets <ivan@dubisoft.solutions>
2023-07-02 12:06:10 +0200

    work on a demo page

---
+++ src/components/DropZone.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+export interface IDroppedFile {
+    file: File;
+    preview: string;
+}
+
+
+interface IDropZone {
+    onSelectFile: (droppedFile: IDroppedFile | undefined) => void | undefined;
+}
+
+const DropZone = (props: IDropZone) => {
+    const [droppedFile, setDroppedFile] = useState<IDroppedFile | undefined>(undefined);
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: {
+            'image/png': ['.png'],
+        },
+        onDrop: (acceptedFiles) => {
+            const file = {file: acceptedFiles[0], preview: URL.createObjectURL(acceptedFiles[0])};
+            setDroppedFile(file);
+            props?.onSelectFile(file)
+        }
+    });
+
+    const clearDroppedFile = () => {
+        setDroppedFile(undefined)
+        props?.onSelectFile(undefined)
+    }
+
+    return (
+        <div className="drop-zone-container">
+            {droppedFile ? (
+                <div className="dropped-file-preview">
+                    <img src={droppedFile.preview} alt={droppedFile.file.name} />
+                    <div className="dropped-file-details d-flex justify-content-center">
+                        <span className='dropped-file-name'>{droppedFile.file.name}</span> 
+                        <button className='btn p-0' onClick={() => clearDroppedFile()}>
+                            <FontAwesomeIcon icon={faTrash} className='ms-3 text-danger' />
+                        </button>
+                    </div>
+                    
+                </div>
+            ) : (
+                <div
+                    {...getRootProps({ className: "dropzone p-5 bg-success" })}
+                >
+                    <input {...getInputProps()} />
+                    <p>Drag 'n' drop some files here, or click to select files</p>
+                    <div>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default DropZone;
--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -1,16 +1,24 @@
+import { useState } from 'react';
+
+
+import DropZone, { IDroppedFile } from '../components/DropZone';
+
+
+
 const DemoPage = () => {
+    const [selectedFile, setSelectedFile] = useState<IDroppedFile|undefined>(undefined)
+    const [step, setStep] = useState(0)
+
     return (
-        <>
         <div className="content d-flex w-100 h-100 mx-auto flex-column">
             <div className="container">
-
                 <div className="row justify-content-center">
                     <div className="col-lg-10 text-center pb-4">
                         <h2 className="mb-lg-4 mb-3">Protect your content</h2>
                         <p className="fs-3 mb-lg-5 mb-4">Add an invisible permanent digital signature that will withstand the challenges posed by generative AI models.</p>
                         
                         <div className="steps mb-3">
-                            <div className="step active">
+                            <div className={"step " + (step == 0 ? 'active' : '')}>
                                 <div className="step-body">
                                     <div className="step-title">
                                         Step 1
@@ -21,7 +29,7 @@ const DemoPage = () => {
                                 </div>
                             </div>
                             
-                            <div className="step">
+                            <div className={"step " + (step == 1 ? 'active' : '')}>
                                 <div className="step-body">
                                     <div className="step-title">
                                         Step 2
@@ -32,7 +40,7 @@ const DemoPage = () => {
                                 </div>
                             </div>
                             
-                            <div className="step">
+                            <div className={"step " + (step == 2 ? 'active' : '')}>
                                 <div className="step-body">
                                     <div className="step-title">
                                         Step 3
@@ -46,19 +54,38 @@ const DemoPage = () => {
 
                         <div className="card">
                             <div className="card-body">
-                                <h3 className="text-start">Upload your content</h3>
-                                <div className="text-end">
-                                    <a href="#" className="btn btn-info disabled">Next</a>
-                                </div>
+
+                                {step == 0 && 
+                                    <div className="step-body">
+                                        <h3 className="text-start">Upload your content</h3>
+                                        <DropZone onSelectFile={(droppedFile) => setSelectedFile(droppedFile)}/>
+                                    
+                                        <div className="text-end mt-3">
+                                            <button className={"btn btn-info" + (!selectedFile ? " disabled" : '')} onClick={() => setStep(1)}>Next</button>
+                                        </div>
+                                    </div>
+                                }
+                                {step == 1 && 
+                                    <div className="step-body">
+                                        <h3 className="text-start">Assign your signature</h3>
+                                    
+                                        <div className="text-end mt-3">
+                                            <button className={"btn btn-info" + (!selectedFile ? " disabled" : '')} onClick={() => setStep(2)}>Next</button>
+                                        </div>
+                                    </div>
+                                }
+                                {step == 2 && 
+                                    <div className="step-body">
+                                        <h3 className="text-start">Download your content</h3>
+                                        
+                                    </div>
+                                }
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        
-        
-        </>
     )
 }
 
